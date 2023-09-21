ac0e4b14a249265ee3e34bdc324ded9987f6e5f0
Gabriel José de Oliveira <gaoliveira211999@gmail.com>
2023-09-21 10:25:28 -0300

    feat: read and split line from stdin

--- main/gsh.go
+++ main/gsh.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 func getCwd() string {
 	ex, err := os.Executable()
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	cwd := filepath.Dir(ex)
@@ -17,9 +20,33 @@ func getCwd() string {
 	return cwd
 }
 
+func readLine() string {
+	reader := bufio.NewReader(os.Stdin)
+
+	line, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return line
+}
+
+func splitLine(line string) []string {
+	args := strings.Fields(line)
+
+	return args
+}
+
 func main() {
+	log.SetPrefix("gsh: ")
+	log.SetFlags(0)
+
 	cwd := getCwd()
 
 	fmt.Printf("%s> ", cwd)
 
+	line := readLine()
+	args := splitLine(line)
+
+	fmt.Println(args)
 }
