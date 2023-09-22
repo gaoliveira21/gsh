a6968365870d9233da774fc31e6a42f07ff2719a
Gabriel José de Oliveira <gaoliveira211999@gmail.com>
2023-09-22 15:58:19 -0300

    feat: add help builtin func

--- builtins/builtins.go
+++ builtins/builtins.go
@@ -1,6 +1,7 @@
 package builtins
 
 import (
+	"fmt"
 	"os"
 	"strings"
 )
@@ -21,9 +22,26 @@ func exit(args ...string) error {
 	return nil
 }
 
+func help(args ...string) error {
+	fmt.Println("*** G Shell ***")
+	fmt.Println("Type program names and arguments and press enter.")
+	fmt.Println("The following commands are built in:")
+
+	builtinsMap := GetMap()
+
+	for key := range builtinsMap {
+		fmt.Printf(" %s\n", key)
+	}
+
+	fmt.Println("Use the man command for information on other programs.")
+
+	return nil
+}
+
 func GetMap() map[string]builtinsfn {
 	return map[string]builtinsfn{
 		"cd":   cd,
 		"exit": exit,
+		"help": help,
 	}
 }
